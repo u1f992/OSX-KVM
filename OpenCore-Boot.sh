00bf8f04bdabfb0a1cf5121145fd99a17df308ab
Koutaro Mukai <mukai.k1011k@outlook.jp>
2026-04-01 12:14:24 +0900

    Add libvirt XML and adjust resources for i9-9900KF host

--- OpenCore-Boot.sh
+++ OpenCore-Boot.sh
@@ -23,10 +23,11 @@ MY_OPTIONS="+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 # This script works for Big Sur, Catalina, Mojave, and High Sierra. Tested with
 # macOS 10.15.6, macOS 10.14.6, and macOS 10.13.6.
 
-ALLOCATED_RAM="4096" # MiB
-CPU_SOCKETS="1"
+ALLOCATED_RAM="65536" # MiB
+CPU_SOCKETS="3"
 CPU_CORES="2"
-CPU_THREADS="4"
+CPU_THREADS="2"
+CPU_TOTAL="12"
 
 REPO_PATH="."
 OVMF_DIR="."
@@ -38,7 +39,7 @@ args=(
   -machine q35
   -device qemu-xhci,id=xhci
   -device usb-kbd,bus=xhci.0 -device usb-tablet,bus=xhci.0
-  -smp "$CPU_THREADS",cores="$CPU_CORES",sockets="$CPU_SOCKETS"
+  -smp "$CPU_TOTAL",cores="$CPU_CORES",sockets="$CPU_SOCKETS",threads="$CPU_THREADS",maxcpus="$CPU_TOTAL"
   -device usb-ehci,id=ehci
   # -device usb-kbd,bus=ehci.0
   # -device usb-mouse,bus=ehci.0
